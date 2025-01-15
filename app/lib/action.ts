6300c4146925423946302e49e624f68d458434c9
Allauddin <khanallauddin113@gmail.com>
2025-01-15 15:27:44 +0500

    sdcds

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -7,29 +7,27 @@ import { z } from "zod";
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
-import { signIn } from '@/auth';
-import { AuthError } from 'next-auth';
- 
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
+
 export async function authenticate(
   prevState: string | undefined,
-  formData: FormData,
+  formData: FormData
 ) {
   try {
-    await signIn('credentials', formData);
+    await signIn("credentials", formData);
   } catch (error) {
     if (error instanceof AuthError) {
       switch (error.type) {
-        case 'CredentialsSignin':
-          return 'Invalid credentials.';
+        case "CredentialsSignin":
+          return "Invalid credentials.";
         default:
-          return 'Something went wrong.';
+          return "Something went wrong.";
       }
     }
     throw error;
   }
 }
-
-
 
 const FormSchema = z.object({
   id: z.string(),
@@ -87,7 +85,9 @@ export async function createInvoice(prevState: State, formData: FormData) {
       message: `${error} Database Error: Failed to Create Invoice.`,
     };
   }
-  revalidatePath("/dashboard/invoices"); // you're updating the data displayed in the invoices route, you want to clear this cache and trigger a new request to the server.
+  revalidatePath("/dashboard/invoices");
+  //Next.js has a Client-side Router Cache that stores the route segments in the user's browser for a time. Along with prefetching, this cache ensures that users can quickly navigate between routes while reducing the number of requests made to the server.
+  // Since you're updating the data displayed in the invoices route, you want to clear this cache and trigger a new request to the server. You can do this with the revalidatePath function from Next.js
   redirect("/dashboard/invoices");
 }
 
